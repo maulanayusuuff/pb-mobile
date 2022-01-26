1e04073cf1b9b60f0c903ea07a6226949134c110
maulanayusuuff <05211840000076@mahasiswa.integra.its.ac.id>
2022-01-27 00:02:36 +0700

    Commit 3

---
+++ Test Suites/Login - Beli Paket.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login - Beli Paket</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>04fb57a5-cce1-46f2-9927-d5d2ca18b9b2</testSuiteGuid>
+</TestSuiteEntity>
